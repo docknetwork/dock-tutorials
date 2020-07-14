83dbf3240bb5ed55b4db254078847a14e564f64c
Sam Hellawell <sshellawell@gmail.com>
2020-07-14 17:52:40 +0100

    add custom ethr resolver class
    Signed-off-by: Sam Hellawell <sshellawell@gmail.com>

---
+++ did-resolving/ethr-resolver.js
@@ -0,0 +1,37 @@
+// Import the DID resolver base class
+import {
+  DIDResolver,
+} from '@docknetwork/sdk/resolver';
+
+import {
+  NoDIDError
+} from '@docknetwork/sdk/utils/did';
+
+import ethr from 'ethr-did-resolver';
+
+// Infura's Ethereum provider for the main net
+const ethereumProviderConfig = {
+  networks: [
+    {
+      name: 'mainnet',
+      rpcUrl: 'https://mainnet.infura.io/v3/05f321c3606e44599c54dbc92510e6a9',
+    },
+  ],
+};
+
+// Custom ethereum resolver class
+export default class EtherResolver extends DIDResolver {
+  constructor(config) {
+    super();
+    this.ethres = ethr.getResolver(config).ethr;
+  }
+
+  async resolve(did) {
+    const parsed = this.parseDid(did);
+    try {
+      return await this.ethres(did, parsed);
+    } catch (e) {
+      throw new NoDIDError(did);
+    }
+  }
+}
