8fb6893b5900e641f76fe16c917956dd1789b9a6
Sam Hellawell <sshellawell@gmail.com>
2020-07-14 16:47:55 +0100

    DID creation tutorial code

---
+++ did-creation/index.js
@@ -0,0 +1,83 @@
+// Import some utils from Polkadot JS
+import { randomAsHex } from '@polkadot/util-crypto';
+
+// Import the dock SDK
+import dock from '@docknetwork/sdk';
+
+// Import some helper methods from the SDK
+import {
+  createNewDockDID,
+	createKeyDetail,
+  createSignedKeyUpdate,
+  createSignedDidRemoval,
+} from '@docknetwork/sdk/utils/did';
+
+import {
+  getPublicKeyFromKeyringPair,
+} from '@docknetwork/sdk/utils/misc';
+
+// Import some shared variables
+import { address, secretUri } from '../shared-constants';
+
+// DID will be generated randomly
+const dockDID = createNewDockDID();
+
+// Generate first key with this seed. The key type is Sr25519
+const keySeed = randomAsHex(32);
+
+// Generate second key (for update) with this seed. The key type is Ed25519
+const secondKeySeed = randomAsHex(32);
+
+// Method from intro tutorial to connect to a node
+async function connectToNode() {
+	await dock.init({ address });
+	const account = dock.keyring.addFromUri(secretUri);
+	dock.setAccount(account);
+  console.log('Connected to the node and ready to go!');
+}
+
+// Method to write the DID
+async function writeDID() {
+  // Generate keys for the DID.
+  const keyPair = dock.keyring.addFromUri(keySeed, null, 'sr25519');
+  const publicKey = getPublicKeyFromKeyringPair(keyPair);
+
+  // Create a key detail, controller being same as the DID
+  const keyDetail = createKeyDetail(publicKey, dockDID);
+  await dock.did.new(dockDID, keyDetail);
+}
+
+// Method to update the DID's key
+async function updateDID() {
+  // Sign key update with this key pair as this is the current key of the DID
+  const currentPair = dock.keyring.addFromUri(keySeed, null, 'sr25519');
+
+  // Update DID key to the following
+  const newPair = dock.keyring.addFromUri(secondKeySeed, null, 'ed25519');
+  const newPk = getPublicKeyFromKeyringPair(newPair);
+
+  // Generate a random new controller ID
+  const newController = randomAsHex(32);
+  const [keyUpdate, signature] = await createSignedKeyUpdate(dock.did, dockDID, newPk, currentPair, newController);
+  await dock.did.updateKey(keyUpdate, signature);
+}
+
+// Method to remove the DID from chain
+async function removeDID() {
+  // Sign the DID removal with this key pair as this is the current key of the DID
+  const currentPair = dock.keyring.addFromUri(secondKeySeed, null, 'ed25519');
+  const [didRemoval, signature] = await createSignedDidRemoval(dock.did, dockDID, currentPair);
+  await dock.did.remove(didRemoval, signature);
+}
+
+// Run!
+async function main() {
+  await connectToNode();
+  await writeDID();
+  await updateDID();
+  await removeDID();
+  await dock.disconnect();
+}
+
+main()
+  .then(() => process.exit(0));
